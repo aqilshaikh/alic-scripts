0c0a04d923a188ffb6ffe560d0e2df9111cb0d0e
Aqil Shaikh <aqilshaikh94@gmail.com>
2021-02-21 09:35:05 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,2 @@
-provider "aws" {
-  region  = "ap-southeast-1"
-}
-
-data "aws_vpc" "selected" {
-  default = true
-}
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-data "aws_subnet_ids" "example" {
-  vpc_id = data.aws_vpc.selected.id
-}
 variable "INSTANCE_USERNAME" {}
 variable "INSTANCE_PASSWORD" {}
